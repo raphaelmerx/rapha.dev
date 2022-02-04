6d6fa805bf11830c462e710625226bd49fd3ac42
Raphael Merx <raphael.merx@gmail.com>
2022-02-04 18:33:01 +0800

    Remove typescript, custom timeline element

--- components/timeline/renderer/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Timeline } from './Timeline'
-export { default as TimelineOSSRenderer } from './TimelineOSSRenderer'
-export { default as TimelinePostRenderer } from './TimelinePostRenderer'
-export { default as TimelineWorkRenderer } from './TimelineWorkRenderer'
